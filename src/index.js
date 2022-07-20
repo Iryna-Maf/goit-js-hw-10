47c1f047aedf7dbd41a1d35de57643ffa3dc3e4f
Iryna-Maf <100190659+Iryna-Maf@users.noreply.github.com>
2022-07-20 14:16:55 +0300

    finished version

--- src/index.js
+++ src/index.js
@@ -39,13 +39,10 @@ function renderContent(countries) {
     renderCountryList(countries);
     return;
   }
+  renderCountryList(countries);
   renderCountry(countries[0]);
 }
 
-// function renderCountryList(countries) {
-//   const murkup = countriesCardTemplate(countries);
-//   countryList.innerHTML = murkup;
-// }
 function renderCountryList(countries) {
   const markup = countries
     .map(({ name, flags: { svg } } = {}) => {
@@ -53,32 +50,23 @@ function renderCountryList(countries) {
 
             <img src = "${svg}" height="200" width="200"/>
 
-          <p><b>Country</b>: ${name}</p>
+          <p><b>Country</b>: ${name.common}</p>
         </li>`;
     })
     .join('');
   countryList.innerHTML = markup;
 }
 
-// function renderCountry(countries) {
-//   countries.languages = Object.values(countries.languages).join(', ');
-//   const country = countryCardTemplate(countries);
-//   countryInfo.innerHTML = country;
-// }
-function renderCountry({
-  name,
-  capital,
-  population,
-  flags: { svg },
-  languages: [{ name: lang }],
-} = {}) {
-  const countryContent = `<div class="country style">
-  <img src='${svg}' height='40' width='60' class='flag_country' />
-  <p class='name'>${name}</p>
-</div>
-<p><b>Capital</b>: ${capital}</p>
-<p><b>Population</b>: ${population}</p>
-<p><b>Languages</b>: ${lang}`;
+function renderCountry(obj) {
+  console.log(obj);
+  const { capital, population, languages } = obj;
+
+  console.log('Hello');
+  const countryContent = `<div class="country style">  
+  <p><b>Capital</b>:${capital}</p>
+<p><b>Population</b>:${population} </p>
+<p><b>Languages</b>:${Object.values(obj.languages)};
+</div>`;
 
   countryInfo.innerHTML = countryContent;
 }
